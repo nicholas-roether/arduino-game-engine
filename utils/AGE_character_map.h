5b9f227eda9d3fc88852b7fb3c65b0b8ca7819d2
nicholas-roether <nicholas.roether@t-online.de>
2022-02-11 21:34:36 +0100

    Remove problematic constants

--- utils/AGE_character_map.h
+++ utils/AGE_character_map.h
@@ -3,10 +3,6 @@
 #include <Arduino.h>
 
 namespace AGE {
-	constexpr uint16_t ARROW_LEFT = '←';
-	constexpr uint16_t ARROW_RIGHT = '→';
-	constexpr uint16_t BLOCK = '∎';
-
 	namespace Utils {
 		uint8_t getCharCode(char character);
 
