9d84e6fe82609d2a05f007dd39c37385abdcd06a
nicholas-roether <nicholas.roether@t-online.de>
2022-02-11 23:42:51 +0100

    stuff

--- AGE_text.cpp
+++ AGE_text.cpp
@@ -62,8 +62,8 @@ namespace AGE::Utils {
 
 	char32_t UnicodeString::charAt(size_t i) const {
 		if (i >= len) return 0x00000000;
-		char character;
-		memcpy(&character, charPtr + i, 1);
+		char32_t character;
+		memcpy(&character, charPtr + i * charSize, charSize);
 		return character;
 	}
 
