6109f1349a08e79b76c7d89e399db068cec8f701
KaranSingh1412 <ksinghsoni306@gmail.com>
2022-02-14 14:16:20 +0100

    Group methods implementation

--- AGE.cpp
+++ AGE.cpp
@@ -8,5 +8,20 @@ namespace AGE {
 		callbackListBuffer.push(Array<void*>());
 	}
 
-	
+	void Group::add(const Component& child) {
+		buffer.push(child);
+		didChange = true;
+	}
+
+	void Group::draw(const LiquidCrystal& lcd) {
+		for (Component child : buffer) child.draw(lcd);
+	}
+
+	void Group::didRedraw() {
+		didChange = false;
+	}
+
+	bool Group::shouldRedraw() {
+		return didChange;
+	}
 }
