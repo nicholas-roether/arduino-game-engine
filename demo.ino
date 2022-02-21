1864c3857d56a846331d6b612cab771b73df4e35
nicholas_roether <nicholas.roether@t-online.de>
2022-02-21 16:24:15 +0100

    new demo

--- demo.ino
+++ demo.ino
@@ -1,42 +1,46 @@
 #include <LiquidCrystal.h>
 
-#define AGE_DEBUG
-
 #include "src/AGE.h"
 #include "src/AGE_structures.h"
 
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
-AGE::EventManager eventManager;
-const AGE::Event TEST_EVT = eventManager.newEvent();
+AGE::Renderer renderer(16, 2);
 
-AGE::Group group;
+class TestComponent : public AGE::Component {
+	unsigned int time = 0;
+	unsigned int lastTime = 0;
+	AGE::Text text = { "", 0, 0 };
 
-void onEvent() {
-	group.draw(lcd);
-}
+public:
+	void build() {
+		String numStr(time);
+		text = AGE::Text(numStr.c_str(), 0, 0);
+		addChild(&text);
+	}
+
+	bool shouldRebuild() {
+		return time != lastTime;
+	}
+
+	void didRebuild() {
+		lastTime = time;
+	}
+
+	void draw(AGE::CharacterBuffer& buffer) {
+		time++;
+	}
+};
+
+TestComponent testComponent;
 
 void setup() {
 	lcd.begin(16, 2);
 
-	Serial.begin(115200);
-	AGE::Utils::Buffer buffer;
-	// group.add(AGE::Text(u"ナニカ　カ゛　オコッタ。", 0, 0));
-	// group.add(AGE::Text(u"コレ、イガイト　ムス゛カシイ。", 0, 1));
-
-	// AGE::Text* text2 = (AGE::Text*) malloc(sizeof(AGE::Text));
-	AGE::Text text = AGE::Text(u"ナニカ　カ゛　オコッタ。", 0, 0);
-	AGE::Text* textPtr = (AGE::Text*) malloc(sizeof(text));
-	// *textPtr = text;
-	memcpy(textPtr, &text, sizeof(text));
-	delay(100);
-	textPtr->draw(lcd);
-	
-	// eventManager.on(TEST_EVT, onEvent);
-
-	// eventManager.dispatch(TEST_EVT);
+	renderer.setRoot(&testComponent);
 }
 
 void loop() {
-	
+	renderer.render(lcd);
+	delay(100);
 }
