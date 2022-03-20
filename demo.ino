ab0f81169c40a8d5766e508dfed482c01f119c73
nicholas-roether <nicholas.roether@t-online.de>
2022-03-20 03:02:18 +0100

    add a huge amount of comments to the demo code

--- demo.ino
+++ demo.ino
@@ -37,6 +37,7 @@ enum Scene {
 	GAME_OVER_SCENE
 };
 
+// Texture creation
 AGE::TextureID TEX_PLAYER_SPACESHIP = process.createTexture({
 	B00000,
 	B11100,
@@ -103,10 +104,16 @@ AGE::TextureID TEX_ENEMY_2 = process.createTexture({
 	B00000
 });
 
-unsigned int score = 0;
+unsigned int score = 0; // The player's current score
 
-AGE::SaveData<unsigned int> highScore;
+AGE::SaveData<unsigned int> highScore; // The player's high score, saved between games
 
+/**
+ * @brief A class for stuff flying across the screen
+ * 
+ * @details A Base class for Bullet, Enemy, and Obstacle - Projectiles move across the
+ * 		Screen horizontally at a fixed y position, with a fixed x velocity.
+ */
 class Projectile : public AGE::SpawnableComponent, public AGE::Collider {
 protected:
 	float xPos;
@@ -121,7 +128,7 @@ public:
 
 	void update(unsigned int dt) {
 		xVel.update(dt, xPos);
-		if (xPos < 0 || xPos >= process.getWidth()) die();
+		if (xPos < 0 || xPos >= process.getWidth()) die(); // die when the projectile leaves the screen
 	}
 
 	AGE::Position getPos() {
@@ -129,6 +136,9 @@ public:
 	}
 };
 
+/**
+ * @brief The bullets the player shoots
+ */
 class Bullet : public Projectile {
 public:
 	Bullet(uint8_t yPos)
@@ -139,15 +149,18 @@ public:
 	}
 
 	void update(unsigned int dt) {
-		Projectile::update(dt);
-		if (collides(OBSTACLE_COLLIDER)) die();
-		if (collides(ENEMY_COLLIDER)) die();
+		Projectile::update(dt); // base class needs to be updated explicitly
+		// Bullets disappear when hitting anything
+		if (collides(OBSTACLE_COLLIDER) || collides(ENEMY_COLLIDER)) die();
 	}
 };
 
+/**
+ * @brief The component responsible for spawning the bullets the player shoots
+ */
 class BulletSpawner : public AGE::Component {
-	uint8_t* yPos;
-	AGE::Spawner spawner = { 3 };
+	uint8_t* yPos; // Points to the yPos member of the Player component
+	AGE::Spawner spawner = { 3 }; // Max. 3 Bullets at a time
 
 public:
 	BulletSpawner(uint8_t* yPos) : yPos(yPos) {}
@@ -161,6 +174,9 @@ public:
 	}
 };
 
+/**
+ * @brief The flashing fire part of the player sprite
+ */
 class PlayerFire : public AGE::Component {
 	uint8_t* yPos;
 	AGE::Animation animation = 200;
@@ -173,6 +189,12 @@ public:
 	}
 };
 
+/**
+ * @brief The Player sprite.
+ * 
+ * @details The Player component includes the spaceship texture, the PlayerFire and the BulletSpawner,
+ * 		and is responsible for initiating a game over and managing the high score.
+ */
 class Player : public AGE::Component, public AGE::Collider {
 	uint8_t yPos = 0;
 
@@ -192,10 +214,11 @@ public:
 	}
 
 	void update(unsigned int dt) {
-		if (upTrigger.fired() && yPos != 0) yPos--;
-		if (downTrigger.fired() && yPos != 3) yPos++;
+		if (upTrigger.fired() && yPos != 0) yPos--; // Move up
+		if (downTrigger.fired() && yPos != 3) yPos++; // Move down
+		// Game over when colliding with an obstacle or enemy
 		if (collides(OBSTACLE_COLLIDER) || collides(ENEMY_COLLIDER)) {
-			if (score > highScore.get()) highScore.set(score);
+			if (score > highScore.get()) highScore.set(score); // Update high score
 			process.setScene(GAME_OVER_SCENE);
 		}
 	}
@@ -205,40 +228,55 @@ public:
 	}
 };
 
+/**
+ * @brief The indestructable obstacles flying towards the player
+ */
 class Obstacle : public Projectile {
 public:
 	Obstacle(uint8_t yPos)
-		: Projectile(process.getWidth() - 1, yPos, -8, OBSTACLE_COLLIDER) {}
+		: Projectile(process.getWidth() - 1, yPos, -8, OBSTACLE_COLLIDER) {} // Speed of -8 characters/second
 
 	void draw(AGE::CharacterBuffer& charBuffer) {
 		charBuffer.put(TEX_OBSTACLE, xPos, yPos);
 	}
 };
 
+/**
+ * @brief The killable enemies flying towards the player.
+ * 
+ * @details Enemy components are also responsible for updating the player's score
+ *		when they are killed.
+ */
 class Enemy : public Projectile {
-	AGE::Animation animation = { 1000 };
+	AGE::Animation animation = { 1000 }; // Animation period of 1000ms
 
 public:
 	Enemy(uint8_t yPos)
 		: Projectile(process.getWidth() - 1, yPos, -8, ENEMY_COLLIDER) {}
 
 	void draw(AGE::CharacterBuffer& charBuffer) {
+		// Use different textures depending on the animation progress
 		charBuffer.put(animation.progress() < 0.5 ? TEX_ENEMY_1 : TEX_ENEMY_2, xPos, yPos);
 	}
 
 	void update(unsigned int dt) {
 		Projectile::update(dt);
+		// Enemies die when hit by a bullet from the player.
 		if (collides(BULLET_COLLIDER)) {
-			score += 100;
+			score += 100; // 100 score points!
 			die();
 		}
 	}
 };
 
+// Fires on average 0.4 times per second.
 AGE::RandomTrigger obstacleSpawnTrigger = { 0.4 };
 
+/**
+ * @brief The component responsible for spawning obstacles and enemies.
+ */
 class ObstacleSpawner : public AGE::Component {
-	AGE::Spawner spawner = { 8 };
+	AGE::Spawner spawner = { 8 }; // Max. 8 obstacles and/or enemies at a time.
 	
 public:
 	void build() {
@@ -247,6 +285,7 @@ public:
 
 	void update(unsigned int dt) {
 		if (obstacleSpawnTrigger.fired()) {
+			// Different spawn patterns
 			switch(random(12)) {
 				case 0:
 					spawner.spawn(Obstacle(0));
@@ -317,6 +356,9 @@ public:
 	}
 };
 
+/**
+ * @brief A component displaying the player's current score in the top right corner (used in GameScene)
+ */
 class ScoreDisplay : public AGE::Component {
 public:
 	void draw(AGE::CharacterBuffer& charBuffer) {
@@ -325,6 +367,10 @@ public:
 	}
 };
 
+
+/**
+ * @brief The scene in which gameplay occurs.
+ */
 class GameScene : public AGE::Component {
 	Player player;
 	ObstacleSpawner obstacleSpawner;
@@ -332,7 +378,7 @@ class GameScene : public AGE::Component {
 
 public:
 	GameScene() {
-		score = 0;
+		score = 0; // reset the score when the scene starts
 	}
 
 	void build() {
@@ -342,39 +388,60 @@ public:
 	}
 };
 
+/**
+ * @brief The menu scene visible on game startup
+ */
 class MenuScene : public AGE::Component {
 public:
 	void draw(AGE::CharacterBuffer& charBuffer) {
+		// TODO proper text alignment api
 		charBuffer.write("SPACE DEMO", process.getWidth() / 2 - 5, 1);
 		charBuffer.write("shoot to start", process.getWidth() / 2 - 7, 2);
 	}
 
 	void update(unsigned int dt) {
+		// Start gameplay when shoot button is pressed
 		if (shootTrigger.fired()) process.setScene(GAME_SCENE);
 	}
 };
 
+/**
+ * @brief A component displaying the player's score, blinking, in the center of the 3rd row
+ * 		(Used in GameOverScene).
+ */
 class LastScore : public AGE::Component {
-	AGE::Animation blinking = { 1000 };
+	AGE::Animation blinking = { 1000 }; // blinking animation period: 1000ms
 
 public:
 	void draw(AGE::CharacterBuffer& charBuffer) {
-		if (blinking.progress() > 0.5) return;
+		if (blinking.progress() > 0.5) return; // Don't display anything 50% of the time
 		String scoreStr = String(score);
+		// TODO proper text alignment api
 		charBuffer.write(scoreStr.c_str(), process.getWidth() / 2 - scoreStr.length() / 2, 1);
 	}
 };
 
+/**
+ * @brief A component displaying the player's saved high score, or "new highscore!" when applicable.
+ */
 class HighScore : public AGE::Component {
 public:
 	void draw(AGE::CharacterBuffer& charBuffer) {
 		String scoreStr;
+		// when the current score equals the saved highscore, a new highscore was achieved.
 		if (score == highScore.get()) scoreStr = "new highscore!";
 		else scoreStr = "HI: " + String(highScore.get());
+		// TODO proper text alignment api
 		charBuffer.write(scoreStr.c_str(), process.getWidth() / 2 - scoreStr.length() / 2, 2);
 	}
 };
 
+/**
+ * @brief The scene that shows when the player dies
+ * 
+ * @details GameOverScene shows a bunch of text, the player's achieved score, and the player's highscore.
+ * 		It also allows the player to retry by pressing the shoot button.
+ */
 class GameOverScene : public AGE::Component {
 	LastScore lastScore;
 	HighScore highScore;
@@ -391,10 +458,12 @@ public:
 	}
 
 	void update(unsigned int dt) {
+		// Retry when pressing shoot
 		if (shootTrigger.fired()) process.setScene(GAME_SCENE);
 	}
 };
 
+// The game class is responsible for choosing the correct scene to show.
 class Game : public AGE::Game {
 protected:
 	AGE::Component* buildScene(AGE::SceneID id) {
@@ -414,17 +483,18 @@ protected:
 	}
 };
 
-Game game;
+Game game; // Game instanciation
 
 void setup() {
-	DEBUG_START;
+	DEBUG_START; // Debug mode init; does nothing unless debug mode is enabled.
 	
+	// Register all triggers
 	process.registerTrigger(&shootTrigger);
 	process.registerTrigger(&upTrigger);
 	process.registerTrigger(&downTrigger);
 	process.registerTrigger(&obstacleSpawnTrigger);
 
-	
+	// Start the game and set startup scene
 	process.start(&game);
 	process.setScene(MENU_SCENE);
 }
