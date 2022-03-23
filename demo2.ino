000e5b51523472e99697fc88c5e2b25752d57616
KaranSingh1412 <ksinghsoni306@gmail.com>
2022-03-23 22:59:16 +0100

    new demo2.ino game

---
+++ demo2.ino
@@ -0,0 +1,44 @@
+#include "src/AGE.h"
+
+AGE::Process snake_process({
+    20, 4,
+    120,
+    9,
+    { 12, 11, 5, 4, 3, 2 }
+});
+
+enum Scene {
+    GAME_SCENE,
+};
+
+class GameScene : public AGE::Component {
+public:
+    void draw(AGE::CharacterBuffer& charBuffer) {
+        charBuffer.write("Hello World", snake_process.getWidth() / 2, snake_process.getHeight() / 2, AGE::CENTER);
+    };
+};
+
+class Game : public AGE::Game {
+protected:
+    AGE::Component* buildScene(AGE::SceneID sceneId) {
+        switch (sceneId)
+        {
+        case GAME_SCENE:
+        return new GameScene();
+            break;
+        default:
+            return nullptr;
+        }
+    };
+};
+
+Game game;
+
+void setup() {
+    snake_process.start(&game);
+    snake_process.setScene(GAME_SCENE);
+}
+
+void loop() {
+    snake_process.loop();
+}
