fc4b653904ab9d40ec677f2e5fad6cda979b32d3
KaranSingh1412 <ksinghsoni306@gmail.com>
2022-03-23 23:22:34 +0100

    new SnakeBody class

--- demo2.ino
+++ demo2.ino
@@ -9,6 +9,34 @@ AGE::Process snake_process({
     { 12, 11, 5, 4, 3, 2 }
 });
 
+AGE::ClickTrigger leftTrigger  = { 7 };
+AGE::ClickTrigger rightTrigger = { 8 };
+
+AGE::TextureID SNAKE_BODY = snake_process.createTexture({
+    B00000,
+    B11111,
+    B11111,
+    B11111,
+    B11111,
+    B11111,
+    B11111,
+    B00000,
+});
+
+class SnakeBody : public AGE::SpawnableComponent {
+    uint8_t xPos;
+    uint8_t yPos;
+
+public:
+    SnakeBody(uint8_t xPos, uint8_t yPos) : xPos(xPos), yPos(yPos) {}
+
+    void draw(AGE::CharacterBuffer& charBuffer) {
+        charBuffer.put(SNAKE_BODY, xPos, yPos);
+    }
+};
+
+
+
 enum Scene {
     GAME_SCENE,
 };
@@ -16,10 +44,12 @@ enum Scene {
 unsigned int score = 0;
 
 class GameScene : public AGE::Component {
+private:
+    SnakeBody snakeBody = { 1, 1 };
 public:
-    void draw(AGE::CharacterBuffer& charBuffer) {
-        charBuffer.write("Hello World", snake_process.getWidth() / 2, snake_process.getHeight() / 2, AGE::CENTER);
-    };
+    void build() {
+        addChild(&snakeBody);
+    }
 };
 
 class Game : public AGE::Game {
